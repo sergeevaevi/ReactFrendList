373b6e19e82950e483e61f91c4d402731aeb2142
elen <elena.sergeeva.99@list.ru>
2022-02-12 19:50:20 +0300

    Initial commit

--- src/FriendList.jsx
+++ src/FriendList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import {find} from "styled-components/test-utils";
 
 const Container = styled.div`
   background: #fff;
@@ -10,24 +11,11 @@ const Container = styled.div`
   max-width: 500px;
   margin: 0 auto;
 `;
-/*<div className="friend-list-item">
-  <img
-      className="friend-list-item__photo"
-      src="https://randomuser.me/api/portraits/med/men/65.jpg"
-  />
-  <div className="friend-list-item__info">
-    <div className="friend-list-item__name">Eugene Pshenichnyy</div>
-    <div className="friend-list-item__username">@pshenichnyy</div>
-  </div>
-  <div className="friend-list-item__mutual">
-    6 mutual
-  </div>
-</div>*/
-export default class FriendList extends React.Component {
-  constructor(props) {
-    super(props);
-      let number = 0;
-      const listItems = props.friends.map((friend) =>
+
+function Friend (props) {
+  const { friend } = props
+  let number = 0;
+  return (
       <div key={number++} className="friend-list-item">
         <img
             className="friend-list-item__photo"
@@ -41,20 +29,52 @@ export default class FriendList extends React.Component {
           {friend.mutualFriends}
         </div>
       </div>
+  )
+}
 
-);
-this.state = {friends: listItems};
+export default class FriendList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {friends: props.friends, hidden : 0};
 
 }
+
   onChange = event => {
-    this.state
+      let hidden = 0;
+      this.state.friends.map((friend) => {
+          let wordsInSearch = 0;
+          let nameWords = friend.name.split(' ');
+          let searchWords = event.target.value.toLowerCase().split(' ');
+          for (let word of nameWords)
+              for (let part of searchWords)
+                  if(word.toLowerCase().startsWith(part)){
+                      wordsInSearch++;
+                      searchWords.splice(searchWords.indexOf(part), 1)
+                      break;
+                  }
+          friend.hide = !(searchWords.length === 0)
+              && !friend.username.toLowerCase().startsWith("@"+event.target.value.toLowerCase())
+          && event.target.value!=="";
+          if(friend.hide) hidden++;
+          return friend;
+      })
+      this.setState((currentState) => {
+          return {
+              friends: currentState.friends,
+              hidden: hidden
+          }
+      })
   }
+
   render() {
+    const friends = this.state.friends;
     return  <Container>
       <div className="header">Friends</div>
       <input onChange={e => this.onChange(e)} className="filter-input" placeholder="Search..."/>
       <div className="friend-list">
-      {this.state.friends}
+          {friends.map(friend => !friend.hide && <Friend  key={friend.username} friend={friend}/>)}
+          {(this.state.hidden === friends.length)
+          && <div className="friend-list-item__none">No friends found</div>}
       </div>
       </Container>;
   }
